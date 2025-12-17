4eb481d032f49f9e16e0aea8ffa82d9b67ea9650
Devaprem1996 <devaprem10@gmail.com>
2025-12-17 09:23:45 +0530

    fixing mobile devices fonts and charcter issues

--- src/services/fileParsing.js
+++ src/services/fileParsing.js
@@ -3,7 +3,7 @@ import * as pdfjsLib from 'pdfjs-dist';
 import mammoth from 'mammoth';
 
 // Configure PDF.js worker
-pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+pdfjsLib.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.js`;
 
 /**
  * Extracts text from various file formats
